50c468f0b60040f58dc734db201296f990928fad
Jahmiro <57642033+Jahmiro@users.noreply.github.com>
2020-06-08 17:50:04 +0200

    Add files via upload

--- Project week 2/src/main/java/Contant.java
+++ Project week 2/src/main/java/Contant.java
@@ -8,7 +8,7 @@ public class Contant extends Betaalwijze {
     /**
      * Methode om betaling af te handelen
      */
-    public boolean betaal(double tebetalen) {
+    public void betaal(double tebetalen) {
         // method body omitted
     }
 }
--- Project week 2/src/main/java/Pinpas.java
+++ Project week 2/src/main/java/Pinpas.java
@@ -14,13 +14,13 @@ public class Pinpas extends Betaalwijze {
      * @param kredietlimiet
      */
     public void setKredietLimiet(double kredietlimiet) {
-        // method body omitted
+        this.kredietlimiet = kredietlimiet;
     }
 
     /**
      * Methode om betaling af te handelen
      */
-    public boolean betaal(double tebetalen) {
+    public void betaal(double tebetalen) {
         // method body omitted
     }
 }
